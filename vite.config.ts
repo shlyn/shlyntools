fe89d8d2024c9be2c55a4c4cd75ab1c528507cfc
shlyn <shp32131@gmail.com>
2022-11-16 15:26:37 +0800

    feat: set project development mode

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,18 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/assets/scss/init.scss";'
+      }
     }
   }
 })
